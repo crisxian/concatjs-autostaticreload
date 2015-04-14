b7a3765fc97a1c3fb05d40ad7d2d33f8bd1acd6e
Christian <crisxian@users.noreply.github.com>
2015-04-14 16:25:58 -0700

    reloading static files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,64 @@
+  // watchserver: updating static file server script initialized variables
+// This is the default port that livereload listens on;
+// change it if you configure livereload to use another port.
+var LIVERELOAD_PORT = 35729;
+// lrSnippet is just a function.
+// It's a piece of Connect middleware that injects
+// a script into the static served html.
+var lrSnippet = require('connect-livereload')({ port: LIVERELOAD_PORT });
+// All the middleware necessary to serve static files.
+var livereloadMiddleware = function (connect, options) {
+  return [
+    // Inject a livereloading script into static files.
+    lrSnippet,
+    // Serve static files.
+    connect.static(options.base[0]),
+    // Make empty directories browsable.
+    connect.directory(options.base[0])
+  ];
+};
+
 
 //Grunt is just JavaScript running in node, after all...
 module.exports = function(grunt) {
 
   // All upfront config goes in a massive nested object.
   grunt.initConfig({
+
+    // Basic grunt updating static file
+      // The connect task is used to serve static files with a local server.
+    connect: {
+      client: {
+        options: {
+          // The server's port, and the folder to serve from:
+          // Ex: 'localhost:9000' would serve up 'client/index.html'
+          port: 9000,
+          base:'client',
+          // Custom middleware for the HTTP server:
+          // The injected JavaScript reloads the page.
+          middleware: livereloadMiddleware
+        }
+      }
+    },
+    // The watch task is used to run tasks in response to file changes
+    watch: {
+      client: {
+        // '**' is used to include all subdirectories
+        // and subdirectories of subdirectories, and so on, recursively.
+        files: ['client/**/*'],
+        // In our case, we don't configure any additional tasks,
+        // since livereload is built into the watch task,
+        // and since the browser refresh is handled by the snippet.
+        // Any other tasks to run (e.g. compile CoffeeScript) go here:
+        tasks:[],
+        options: {
+          livereload:LIVERELOAD_PORT
+        }
+      }
+    },
+
+
+    // Basic grunt concat script
     // You can set arbitrary key-value pairs.
     distFolder: 'dist',
     // You can also set the value of a key as parsed JSON.
@@ -32,13 +87,25 @@ module.exports = function(grunt) {
         // Ex: '<%= concat.options.separator %>' instead of ';'
       }
     }
+
+
   }); // The end of grunt.initConfig
 
   // We've set up each task's configuration.
   // Now actually load the tasks.
   // This will do a lookup similar to node's require() function.
-  grunt.loadNpmTasks('grunt-contrib-concat');
+//  grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Register our own custom task alias.
-  grunt.registerTask('build', ['concat']);
+//  grunt.registerTask('build', ['concat']);
+
+//  grunt.loadNpmTasks('grunt-contrib-connect');
+//  grunt.loadNpmTasks('grunt-contrib-watch');
+
+// replace the above manual load tasks with matchdep
+require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  // Can register two shell commands using an alias
+  grunt.registerTask('preview', ['connect:client','watch:client']);
+
 };
